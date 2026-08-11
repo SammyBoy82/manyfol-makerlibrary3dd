248add19543257fd9e2054ae5589cc7b2e777e4e
sammyboy82 <122948970+SammyBoy82@users.noreply.github.com>
2026-08-12 09:31:13 +1000

    Add nesting review route

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -84,6 +84,10 @@ authenticate :user, lambda { |u| u.is_administrator? } do
     to: "admin/integrity#remove_duplicates",
     as: :admin_integrity_remove_duplicates
 
+  get "/admin/integrity/nesting",
+    to: "admin/integrity#nesting",
+    as: :admin_integrity_nesting
+
   get "/admin/integrity/preview/:category",
     to: "admin/integrity#preview",
     as: :admin_integrity_preview,

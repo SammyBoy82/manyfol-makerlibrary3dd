1442eae4d18f185f7965b6702975c16e6f9fdc5f
sammyboy82 <122948970+SammyBoy82@users.noreply.github.com>
2026-08-12 00:03:23 +1000

    Add integrity preview controller action

--- app/controllers/admin/integrity_controller.rb
+++ app/controllers/admin/integrity_controller.rb
@@ -9,6 +9,15 @@ module Admin
       @report = Admin::IntegrityAnalyzer.call
     end
 
+    def preview
+      skip_policy_scope
+      skip_authorization
+
+      @preview = Admin::IntegrityPreview.call(params[:category])
+    rescue ArgumentError
+      redirect_to admin_integrity_path, alert: "Preview is not available for that integrity category."
+    end
+
     private
 
     def require_administrator!
